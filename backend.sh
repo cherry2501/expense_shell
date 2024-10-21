1ff0ef5bb974055c1ec65559e883ffe43298464d
cherry2501 <saicharangudiya@gmail.com>
2024-10-21 19:01:13 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOG_FOLDER="/var/log/shell-script"
+LOG_FOLDER="/var/log/expense"
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
